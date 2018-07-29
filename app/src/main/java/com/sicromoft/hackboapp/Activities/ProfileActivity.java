f3db56ee71d74911d19912c5042b25d1821e33f1
Kevinsan94 <kevinviscafe@gmail.com>
2018-07-29 02:22:15 -0400

    Logout

--- app/src/main/java/com/sicromoft/hackboapp/Activities/ProfileActivity.java
+++ app/src/main/java/com/sicromoft/hackboapp/Activities/ProfileActivity.java
@@ -1,7 +1,10 @@
 package com.sicromoft.hackboapp.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,6 +24,16 @@ public class ProfileActivity extends AppCompatActivity {
         TextView name;
         name = findViewById(R.id.profile_name);
         name.setText(user.getDisplayName());
+
+        Button logout = findViewById(R.id.logoutButton);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                auth.signOut();
+                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
